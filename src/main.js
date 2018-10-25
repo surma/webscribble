d2527cf3cb6f85b6850364b677430539576dacd1
Surma <surma@surma.link>
2018-10-26 00:30:26 +0200

    Fix layout

--- src/main.js
+++ src/main.js
@@ -58,7 +58,7 @@ async function init() {
     theme: "monokai"
   });
   window.CodeMirror = CodeMirror;
-  editor.setSize("auto", "100%");
+  // editor.setSize("50%", "100%");
   editor.on("change", () => (hasBeenEdited = dirty = true));
   setInterval(updateIframe, 1000);
 
